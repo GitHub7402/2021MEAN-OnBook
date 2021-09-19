dd77749ede817c0d55c0a0760dc08fc986aa7f62
GitHub7402 <77265634+GitHub7402@users.noreply.github.com>
2021-09-19 20:51:59 +0900

    Complete apply-modal

--- js/screen-header.js
+++ js/screen-header.js
@@ -1,7 +1,19 @@
-const backBtn = document.querySelector(".screen-header__column");
+const backBtn = document.querySelector(".screen-header__column .fa-chevron-left");
+const missionBtn = document.querySelector(".screen-header__column .fa-bars");
 
 function goToBack() {
     window.history.back();
 }
 
+function goToMissions() {
+    const a = document.createElement("a");
+    missionBtn.appendChild(a);
+    a.href = "missions.html";
+    a.click();
+}
+
 backBtn.addEventListener("click", goToBack);
+
+if (missionBtn) {
+    missionBtn.addEventListener("click", goToMissions);
+}
---
+++ js/toggle.js
@@ -0,0 +1,91 @@
+// hope.html like toggle w)modal
+const applyBtn1 = document.querySelector("#applyBtn1");
+const applyBtn2 = document.querySelector("#applyBtn2");
+const applyBtn3 = document.querySelector("#applyBtn3");
+const applyModal = document.querySelector(".apply-modal");
+
+const APPLIED = "applied";
+const NOT_APPLIED = "notApplied";
+const A1 = "application1";
+const A2 = "application2";
+const A3 = "application3";
+
+if (localStorage.getItem(A1) === APPLIED) {
+    applyBtn1.children[0].classList.add("applyHeartColor");
+}
+if (localStorage.getItem(A2) === APPLIED) {
+    applyBtn2.children[0].classList.add("applyHeartColor");
+}
+if (localStorage.getItem(A3) === APPLIED) {
+    applyBtn3.children[0].classList.add("applyHeartColor");
+}
+
+function changeColor(event) {
+    if (event.target.children[0]) {
+        event.target.children[0].classList.toggle("applyHeartColor");
+    } else {
+        event.target.classList.toggle("applyHeartColor");
+    }
+}
+
+function removeModal() {
+    applyModal.classList.add("removeContent");
+}
+
+function showModal() {
+    applyModal.classList.remove("removeContent");
+    setTimeout(removeModal, 3000);
+}
+
+function clickApplyBtn1(event) {
+    if (localStorage.getItem(A1) === NOT_APPLIED) {
+        localStorage.setItem(A1, APPLIED);
+        showModal();
+    } else if (localStorage.getItem(A1) === APPLIED) {
+        localStorage.setItem(A1, NOT_APPLIED);
+    }
+    changeColor(event);
+}
+
+function clickApplyBtn2(event) {
+    if (localStorage.getItem(A2) === NOT_APPLIED) {
+        localStorage.setItem(A2, APPLIED);
+        showModal();
+    } else if (localStorage.getItem(A2) === APPLIED) {
+        localStorage.setItem(A2, NOT_APPLIED);
+    }
+    changeColor(event);
+}
+
+function clickApplyBtn3(event) {
+    if (localStorage.getItem(A3) === NOT_APPLIED) {
+        localStorage.setItem(A3, APPLIED);
+        showModal();
+    } else if (localStorage.getItem(A3) === APPLIED) {
+        localStorage.setItem(A3, NOT_APPLIED);
+    }
+    changeColor(event);
+}
+
+if (applyBtn1) {
+    applyBtn1.addEventListener("click", clickApplyBtn1);
+}
+if (applyBtn2) {
+    applyBtn2.addEventListener("click", clickApplyBtn2);
+}
+if (applyBtn3) {
+    applyBtn3.addEventListener("click", clickApplyBtn3);
+}
+
+// missions.html toggle
+const missionsTitle_book1 = document.querySelector(".missions__title--book1");
+const missionsLinks_book1 = document.querySelector(".missions__links--book1");
+
+function missionToggle_book1() {
+    missionsLinks_book1.classList.toggle("removeContent");
+
+}
+
+if (missionsTitle_book1) {
+    missionsTitle_book1.addEventListener("click", missionToggle_book1);
+}
